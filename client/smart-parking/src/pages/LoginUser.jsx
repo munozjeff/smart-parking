bc275577ba04183df3e950e5de6bde501a3be884
vidahoro2 <vidahoro@gmail.com>
2023-06-24 23:21:04 -0500

    carpetas de desarrollo

---
+++ client/smart-parking/src/pages/LoginUser.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { LoginUser } from "../components/LoginUser";
+import './LoginUserPage.css'
+
+function LoginUserPage(){
+
+    return(
+        <div className="main-container">
+            <LoginUser/>
+        </div>
+    )
+}
+
+export {LoginUserPage} 
