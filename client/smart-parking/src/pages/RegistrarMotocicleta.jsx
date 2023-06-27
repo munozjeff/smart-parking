653fa86d157d789d0a88790f05a4009a01fc3eea
munozjeff <jeffersonsteven231997@gmail.com>
2023-06-26 22:41:21 -0500

    bug registro moto solved

--- client/smart-parking/src/pages/RegistrarMotocicleta.jsx
+++ client/smart-parking/src/pages/RegistrarMotocicleta.jsx
@@ -12,7 +12,8 @@ function RegistrarMotocicleta(){
         setForm({...form,[name]:value})
     }
     const handlerSendData=()=>{
-      console.log(form.placa);
+        event.preventDefault()
+        console.log(form);
     }
     
     return(
@@ -27,8 +28,8 @@ function RegistrarMotocicleta(){
             <form onSubmit={handlerSendData} className="form-registro-moto">
 
                 <input id="placa" value={form.placa || ""} name="placa" className="inputs" type="text" placeholder="Ingresa Placa" onChange={handlerFormData}></input>
-                {/* <input id="tarjeta" value={form.tarjeta || ""} name="tarjeta" className="inputs" type="file" placeholder="Tarjeta de Propiedad" alt="imagen" onChange={handlerFormData}></input>
-                <input src="./src/assets/files.png" id="motoImage" value={form.motoImage || ""} name="motoImage" className="inputs" type="file" placeholder="Imagen de la moto" alt="imagen" onChange={handlerFormData}></input> */}
+                <input id="tarjeta" value={form.tarjeta || ""} name="tarjeta" className="inputs" type="file" placeholder="Tarjeta de Propiedad" alt="imagen" onChange={handlerFormData}></input>
+                <input src="./src/assets/files.png" id="motoImage" value={form.motoImage || ""} name="motoImage" className="inputs" type="file" placeholder="Imagen de la moto" alt="imagen" onChange={handlerFormData}></input>
                 <input id="passwordRegister"  value={form.passwordRegister || ""} name="passwordRegister" className="inputs" type="password" placeholder="Verificar Contraseña" onChange={handlerFormData}></input>
                 <button id="btn-submit" type="submit"> Registar Motocicleta</button>
 
