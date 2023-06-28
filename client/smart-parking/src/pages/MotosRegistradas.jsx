e316c8563567b68a64160108703555e3f3b2d64a
vidahoro2 <vidahoro@gmail.com>
2023-06-28 16:39:55 -0500

    QR with User email

--- client/smart-parking/src/pages/MotosRegistradas.jsx
+++ client/smart-parking/src/pages/MotosRegistradas.jsx
@@ -5,20 +5,16 @@ import {app} from '../components/firebase/firebase.jsx'
 import QRCode from 'qrcode.react';
 import {getAuth, onAuthStateChanged } from "firebase/auth";
 
-
-
 const MotosRegistradas = ()=>{
     const [motos, setMotos] = useState([]);
     const [selectedMoto, setSelectedMoto] = useState(null);
     const [user, setUser] = useState([]);
     const [dataUser, setDataUser] = useState(null);
     const [currentUser, setCurrentUser] = useState(null);
-    
 
     useEffect(()=>{
         const motosDB = getFirestore();
         const queryCollection = collection(motosDB,'motos');
-        
 
         getDocs(queryCollection)
         .then((QuerySnapshot)=>{
@@ -29,13 +25,8 @@ const MotosRegistradas = ()=>{
             });
             setMotos(datosMotos);
             console.log(datosMotos);
-        } )
-        // getDocs(queryCollection)
-        // .then(res => setMotos(res.docs.map(moto =>({id:moto.id, ...moto.data()})) ))
-
-
+        })
     },[])
-
 
     useEffect(() => {
         const auth = getAuth();
@@ -51,57 +42,49 @@ const MotosRegistradas = ()=>{
           unsubscribe();
         };
       }, []);
-    
-
 
     return(
-
         <>
-            
             <div className="table-main-container">
                 <div>
                     <h1>Motos Registradas</h1>
                 </div>
                 <table>
-                <thead>
-                    <tr>
-                        <th>Imagen Moto </th>
-                        <th>Placa</th>
-                        <th>Usuario</th>
-                        <th>QR</th>
-
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {motos.map((moto,index)=>(
-                        <tr key={index}>
-                        <td>
-                            <img src={moto.motoImage} alt="MotoImg" width="50" height="50" />
-                        </td>
-                        <td>{moto.placa}</td>
-                        <td>{currentUser ? currentUser.email : ''}</td>
-                        <td><button className="QR-button" onClick={() => setSelectedMoto(moto)}>Generar QR</button></td>
+                    <thead>
+                        <tr>
+                            <th>Imagen Moto </th>
+                            <th>Placa</th>
+                            <th>Usuario</th>
+                            <th>QR</th>
                         </tr>
-                    ))}
-       
+                    </thead>
 
-                </tbody>
-                    
+                    <tbody>
+                        {motos.map((moto, index) => {
+                            const motoWithUser = { ...moto, user: currentUser ? currentUser.email : '' };
+
+                            return (
+                                <tr key={index}>
+                                    <td>
+                                        <img src={moto.motoImage} alt="MotoImg" width="50" height="50" />
+                                    </td>
+                                    <td>{moto.placa}</td>
+                                    <td>{motoWithUser.user}</td>
+                                    <td><button className="QR-button" onClick={() => setSelectedMoto(motoWithUser)}>Generar QR</button></td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
                 </table>
             </div>
             {selectedMoto && (
-            <div>
-                <h2>QR Code for {selectedMoto.placa}</h2>
-                <QRCode value={JSON.stringify(selectedMoto)} />
-            </div>
+                <div>
+                    <h2>QR Code for {selectedMoto.placa}</h2>
+                    <QRCode value={JSON.stringify(selectedMoto)} />
+                </div>
             )}
-
         </>
-    )
-
+    );
 }
 
-
-
-export {MotosRegistradas}
+export {MotosRegistradas};
